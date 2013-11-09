c907a06aeadb5d19dd2140c45e5d6a37350f38e3
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-11-09 17:02:25 +0530

    added routes, route handlers. Basic skelton ready

---
+++ src/wrinqle_app.erl
@@ -0,0 +1,19 @@
+%% Feel free to use, reuse and abuse the code in this file.
+
+%% @private
+-module(wrinqle_app).
+-behaviour(application).
+
+%% API.
+-export([start/2]).
+-export([stop/1]).
+
+%% API.
+start(_Type, _Args) ->
+	Dispatch = cowboy_router:compile(wrinqle_routes:routes_configuration()),
+	{ok, _} = cowboy:start_http(http, 100, [{port, 3000}],
+		[{env, [{dispatch, Dispatch}]}]),
+	wrinqle_sup:start_link().
+
+stop(_State) ->
+	ok.
---
+++ src/wrinqle_routes.erl
@@ -0,0 +1,10 @@
+-module(wrinqle_routes).
+-exports(routes_configuration/0).
+
+routes_configuration()->
+    [{'-',[
+
+	   {"/websocket",wrinqle_socket_handler,[]}
+	  ]
+     }
+    ].
