46f654c2a66b04a1d7470c6dd73586ea54612677
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-11-12 19:04:16 +0530

    refined the helper function add_pid/2

--- src/wrinqle_helpers.erl
+++ src/wrinqle_helpers.erl
@@ -2,9 +2,9 @@
 -export([add_pid/2]).
 
 add_pid(Pid,Name)->
-    case pg2:get_members(Name) of
-	Pid->
-	    ok;
+Member = pg2:get_members(Name),
+    case Member of
+	lists:nth(1,Member)-> ok;
 	{error,_} ->pg2:create(Name),
 		    pg2:join(Name,Pid)
     end.
