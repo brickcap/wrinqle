9a60633a04c2e12b1ccf7693b2813957169e3790
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-11-12 19:10:54 +0530

    A better pattern match for the existing pid

--- src/wrinqle_helpers.erl
+++ src/wrinqle_helpers.erl
@@ -4,7 +4,7 @@
 add_pid(Pid,Name)->
 Member = pg2:get_members(Name),
     case Member of
-	lists:nth(1,Member)-> ok;
+	[Pid]-> ok;
 	{error,_} ->pg2:create(Name),
 		    pg2:join(Name,Pid)
     end.
