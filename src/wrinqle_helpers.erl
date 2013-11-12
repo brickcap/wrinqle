54670d43b48d29c786e5448a8973fdf40e50c8ad
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-11-12 20:22:40 +0530

    Readied a delivery mechanism

--- src/wrinqle_helpers.erl
+++ src/wrinqle_helpers.erl
@@ -1,5 +1,7 @@
 -module(wrinqle_helpers).
+
 -export([add_pid/2]).
+-export([deliver_message/2]).
 
 add_pid(Pid,Name)->
 Member = pg2:get_members(Name),
@@ -8,4 +10,13 @@ Member = pg2:get_members(Name),
 	{error,_} ->pg2:create(Name),
 		    pg2:join(Name,Pid)
     end.
+
+deliver_message(Channel,Msg)-> 
+    Member = pg2:get_members(Channel),
+    case Member of
+	[Pid]-> Pid ! {jiffy:encode({[{status,200}]})};
+	{error,_}->{jiffy:encode({[{status,404}]})}
+    end;
+ 
+deliver_message(Channels,Msg) ->ok.
 
