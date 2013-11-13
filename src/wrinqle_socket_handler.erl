ba39e593fb6d3d97976e3955b5ba82612f0e6e6a
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-11-13 22:46:02 +0530

    Wrote socket_info single and multiple delivery

--- src/wrinqle_socket_handler.erl
+++ src/wrinqle_socket_handler.erl
@@ -36,10 +36,11 @@ websocket_handle({text, Msg}, Req, State) ->
 
 websocket_handle(_Data, Req, State) ->  {ok, Req, State}. 
 
+websocket_info({error,_unavailaible},Req,State)->
+    {reply,{text,jiffy:encode({[{status,404}]})},Req,State};
 
-websocket_info({timeout, _Ref, Msg}, Req, State) ->
-    erlang:start_timer(1000, self(), <<"How' you doin'?">>),
-    {reply, {text, Msg}, Req, State};
+websocket_info({send,Msg},Req,State) ->
+    {reply,{text,jiffy:encode({[{status,200},{msg,Msg}]})},Req,State};
 
 websocket_info(_Info, Req, State) ->
     {ok, Req, State}.
