47b463483956e7691929b1544b6ed4b2009a33ff
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-11-14 12:02:30 +0530

    Try catch in add pid

--- src/wrinqle_helpers.erl
+++ src/wrinqle_helpers.erl
@@ -6,9 +6,10 @@
 -export([publish/2]).
 
 add_pid(Pid,Name)->
-Member = pg2:get_members(Name),
-    case Member of
-	[Pid|_]-> ok;
+
+    try Member = pg2:get_members(Name)
+
+    catch 
 	{error,_} ->pg2:create(Name),
 		    pg2:join(Name,Pid)
 
