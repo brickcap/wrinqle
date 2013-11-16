69281dad867cb5ed22920ca485e95e613f4ff65e
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-11-16 14:59:08 +0530

    Added the default handlers from helper to the wrinqle_event handlers

--- src/wrinqle_event_handler.erl
+++ src/wrinqle_event_handler.erl
@@ -1,3 +1,46 @@
 -module(wrinqle_event_handler).
 -export([init/1]).
 -export([handle_event/2]).
+
+
+init(_Args)->
+    {ok,[]}.
+handle_event(send_message,To,Msg)-> 
+
+    Member = pg2:get_members(To),
+
+
+    case Member of
+	[Pid|_] -> lager:info("Check ~p",Pid),
+		   Pid!{send,Msg};
+	{error,_}-> self()!{error,unavailable}
+    end;
+handle_event(send_message,To,Msg) when is_list(To) ->  
+
+    lists:foreach( 
+      fun(N)->
+	      Member = pg2:get_members(N),
+	      case Member of
+
+		  [Pid,_]->Pid!{send,Msg};
+		  {error,_}-> self()!{error,unavailable}
+	      end
+      end,
+      To);
+
+handle_event(subscribe,To,Channel)->
+    
+    Member = pg2:get_members(To),
+    case Member of
+	[To|_]->pg2:join(Channel),
+		self()!subscribed;
+	{error,_} ->self()!error
+    end;
+handle_event(subscribe,To,Channels) when is_list(Channels) ->
+
+    Member = pg2:get_members(To),
+    case Member of
+	[To|_]-> lists:foreach(fun(N)->pg2:join(N) end),
+		 self()!subscribed;
+	{error,_}-> self()!error
+    end;
