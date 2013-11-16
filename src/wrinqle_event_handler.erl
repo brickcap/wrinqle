f55ea4b8e99740c0ee75336928351efa1668fa55
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-11-16 23:19:00 +0530

    Added dummy handlers to satisfy the compiler

--- src/wrinqle_event_handler.erl
+++ src/wrinqle_event_handler.erl
@@ -2,22 +2,12 @@
 
 -export([init/1]).
 -export([handle_event/2]).
+-export([handle_call/2,terminate/2,handle_info/2,code_change/3]).
 
 -behaviour(gen_event).
 
 init(_Args)->
     {ok,[]}.
-handle_event({send_message,To,Msg},State)-> 
-
-    Member = pg2:get_members(To),
-
-
-    case Member of
-	[Pid|_] -> lager:info("Check ~p",Pid),
-		   Pid!{send,Msg};
-	{error,_}-> self()!{error,unavailable}
-    end
-	{ok,State};
 
 handle_event({send_message,To,Msg},State) when is_list(To) ->  
 
@@ -30,8 +20,34 @@ handle_event({send_message,To,Msg},State) when is_list(To) ->
 		  {error,_}-> self()!{error,unavailable}
 	      end
       end,
-      To)
+      To),
 	{ok,State};
+
+
+handle_event({send_message,To,Msg},State)-> 
+
+    Member = pg2:get_members(To),
+
+
+    case Member of
+	[Pid|_] -> lager:info("Check ~p",Pid),
+		   Pid!{send,Msg};
+	{error,_}-> self()!{error,unavailable}
+    end,
+	{ok,State};
+
+
+
+handle_event({subscribe,To,Channels},State) when is_list(Channels) ->
+
+    Member = pg2:get_members(To),
+    case Member of
+	[To|_]-> lists:foreach(fun(N)->pg2:join(N) end),
+		 self()!subscribed;
+	{error,_}-> self()!error
+    end,
+    {ok,State};
+
 
 handle_event({subscribe,To,Channel},State)->
 
@@ -40,22 +56,26 @@ handle_event({subscribe,To,Channel},State)->
 	[To|_]->pg2:join(Channel),
 		self()!subscribed;
 	{error,_} ->self()!error
-    end
+    end,
 	{ok,State};
-handle_event({subscribe,To,Channels},State) when is_list(Channels) ->
 
-    Member = pg2:get_members(To),
-    case Member of
-	[To|_]-> lists:foreach(fun(N)->pg2:join(N) end),
-		 self()!subscribed;
-	{error,_}-> self()!error
-    end;
-{ok,State};
-handle_event({publish,Channel,Msg})->
+handle_event({publish,Channel,Msg},State)->
 
     Member = pg2:get_members(Channel),
     case Member of 
 	[Channel|_]->Channel!{Channel,Msg};
 	{error,_}-> failed
-    end
+    end,
 	{ok,State}.
+
+handle_call(_, State) ->
+    {ok, ok, State}.
+
+handle_info(_, State) ->
+    {ok, State}.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+terminate(_Reason, _State) ->
+    ok.
