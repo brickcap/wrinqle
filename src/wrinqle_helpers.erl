b72462affc495c55971517556cfd2756d20332e1
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-11-17 12:20:50 +0530

    helpers cleared out

--- src/wrinqle_helpers.erl
+++ src/wrinqle_helpers.erl
@@ -1,9 +1,6 @@
 -module(wrinqle_helpers).
 
 -export([add_pid/2]).
--export([deliver_message/2]).
--export([subscribe/2]).
--export([publish/2]).
 
 add_pid(Pid,Name)->
     lager:info("The name of the channel is~p",[Name]),
@@ -14,51 +11,4 @@ add_pid(Pid,Name)->
 		     lager:info("The members of channel are",pg2:get_members(Name));
 	_->  ok
 end.
-
-deliver_message(To,Msg) when is_list(To),erlang:length(To)>0 ->
-    lists:foreach( 
-      fun(N)->
-	      Member = pg2:get_members(N),
-	      case Member of
-
-		  [Pid,_]->Pid!{send,Msg};
-		  {error,_}-> self()!{error,unavailable}
-	      end
-      end,
-      To);
-
-
-deliver_message(To,Msg)->
-
-     Member = pg2:get_members(To),
-   
-    
-case Member of
-    [Pid|_] -> lager:info("Check ~p",Pid),
-	       Pid!{send,Msg};
-    {error,_}-> self()!{error,unavailable}
-end.
-
-
-subscribe(To,Channels) when  is_list(Channels)-> 
-    Member = pg2:get_members(To),
-    case Member of
-	[To|_]-> lists:foreach(fun(N)->pg2:join(N) end),
-		 self()!subscribed;
-	{error,_}-> self()!error
-    end;
-subscribe(To,Channel) ->
-    Member = pg2:get_members(To),
-    case Member of
-	[To|_]->pg2:join(Channel),
-		self()!subscribed;
-	{error,_} ->self()!error
-    end.
-
-publish(Channel,Msg)-> 
-    Member = pg2:get_members(Channel),
-    case Member of 
-	[Channel|_]->Channel!{Channel,Msg};
-	{error,_}-> failed
-    end.
 
