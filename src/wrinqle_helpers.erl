2f65ee393696765aa9587e910575a71ffd1e2109
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-11-17 12:29:42 +0530

    Added a remove pid function to helpers

--- src/wrinqle_helpers.erl
+++ src/wrinqle_helpers.erl
@@ -3,12 +3,30 @@
 -export([add_pid/2]).
 
 add_pid(Pid,Name)->
-    lager:info("The name of the channel is~p",[Name]),
+    
     Member = pg2:get_members(Name),
-    case Member of 
-	{error,_} ->pg2:create(Name),		   
-		    pg2:join(Name,Pid),
-		     lager:info("The members of channel are",pg2:get_members(Name));
-	_->  ok
-end.
 
+    case Member of
+ 
+	{error,_} ->
+
+	    pg2:create(Name),		   
+	    pg2:join(Name,Pid),
+	    lager:info("The members of channel are",pg2:get_members(Name));
+
+	_->  ok
+
+    end.
+
+remove_pid(Pid,Name)->
+
+    Member = pg2:get_members(Name),
+
+    case Member of
+
+	{error,_} ->ok;
+
+	_->
+
+	    pg2:leave(Pid,Name)  
+    end.
