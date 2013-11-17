769a95693334d276f39dbb64c836b6c4edd15d39
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-11-17 13:07:42 +0530

    Added a method to dry up channel notification

--- src/wrinqle_helpers.erl
+++ src/wrinqle_helpers.erl
@@ -4,20 +4,20 @@
 -export([remove_pid/2]).
 
 add_pid(Pid,Name)->
-    
+
     Member = pg2:get_members(Name),
 
     case Member of
- 
+
 	{error,_} ->
 
 	    pg2:create(Name),		   
 	    pg2:join(Name,Pid),
-	    gen_event:notify(wrinqle_channel_events,pid_registered),
+	    channel_event_notifier(pid_registered),
 	    lager:info("The members of channel are",pg2:get_members(Name));
 
 	_->  
-	    gen_event:notify(wrinqle_channel_events,channel_unavailable)
+	    channel_event_notifier(channel_unavailable)
 
     end.
 
@@ -27,12 +27,15 @@ remove_pid(Pid,Name)->
 
     case Member of
 
-	{error,_} -> gen_event:notify(wrinqle_channel_events,channel_unavailable);
- 
+	{error,_} -> 
+	    channel_event_notifier(channel_unavailable);
 
 	_->
 
 	    pg2:leave(Pid,Name),  
-	    gen_event:notify(wrinqle_channel_events,pid_removed)
+	    channel_event_notifier(removed_pid)
 
     end.
+
+channel_event_notifier(Name)->
+    gen_event:notify(wrinqle_channel_events,Name).
