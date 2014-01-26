29b6406ffffe14a76bd3de4134350383ab7ed036
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-01-26 11:59:21 +0530

    Added rebar and relx executeables within the directory

--- src/wrinqle_app.erl
+++ src/wrinqle_app.erl
@@ -11,7 +11,7 @@
 %% API.
 start(_Type, _Args) ->
 	Dispatch = cowboy_router:compile(wrinqle_routes:routes_configuration()),
-	{ok, _} = cowboy:start_http(http, 100, [{port, 3000}],
+	{ok, _} = cowboy:start_http(http, 100, [{port, 4000}],
 				    [{env, [{dispatch, Dispatch}]}]),
     pg2:start(),
 
