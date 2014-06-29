382cdd2f4f11e6777e488b6c17e11dc2fc6b533b
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-06-29 17:02:13 +0530

    Working with 100 sockets at a time!

--- example/script.js
+++ example/script.js
@@ -1,13 +1,19 @@
+var sockets=[];
+var info = document.getElementById("socketInfo");
+for(var i=0;i<=100;i++){
+   console.log(i.toString()); 
+    sockets.push(new WebSocket("ws://localhost:4000/websocket/socket"+i.toString()));
+    sockets[i].onopen = function(){
+	if(i>4){
+	    sockets[0].send(JSON.stringify({"to":["socket3","socket4"],"msg":"Hey there"}));
+	}
+    };
+    sockets[i].onmessage = function(data){
+	info.innerHTML+="<li>"+i.toString()+": "+data.data+"</li>";
+    };
 
-var socket = new WebSocket("ws://localhost:4000/websocket/test");
 
-socket.onopen = function(){
-    console.log("socket has been opened");
-};
-socket.onmessage = function(data){
-    console.log(data);
-};
+}
 
-window.onbeforeunload = function(){
-    socket.disconnect();
-};
+
+
